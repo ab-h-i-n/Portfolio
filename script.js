6991607c147894a325d019e4695ad0ccbd4e5102
ab-h-i-n <abhinshelly@gmail.com>
2024-02-16 19:50:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    const hamburgerBtn = document.querySelector('.hamburger-btn');
+    const menuBar = document.querySelector('.menu-bar');
+
+    hamburgerBtn.addEventListener("click", () => {
+        menuBar.classList.toggle('hidden');
+
+    });
+
